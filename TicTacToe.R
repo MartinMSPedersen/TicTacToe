2a233f5ca7ebce1abf511e248b8e8286714caf01
Martin M. S. Pedersen <traxplayer@gmail.com>
2018-11-01 12:26:19 +0100

    initial commit

---
+++ TicTacToe.R
@@ -0,0 +1,59 @@
+setwd("/home/bruger1/tictactoe")
+source("TicTacToeBoard.R")
+
+b <- TicTacToeBoard()
+for (move1 in b$legalMoves) {
+  b <- makeMove(b, move1)
+  for (move2 in b$legalMoves) {
+    b <- makeMove(b, move2)
+    for (move3 in b$legalMoves) {
+      b <- makeMove(b, move3)
+      for (move4 in b$legalMoves) {
+        b <- makeMove(b, move4)
+        for (move5 in b$legalMoves) {
+          b <- makeMove(b, move5)
+          if (b$isGameOver) {
+            print(b)
+            cat(b$moves)
+            cat("\n")
+            b <- undoMove(b,move5)
+            next
+          }
+          for (move6 in b$legalMoves) {
+            b <- makeMove(b, move6)
+            if (b$isGameOver) {
+              cat(b$moves)
+              cat("\n")
+              b <- undoMove(b,move6)
+              next
+            }
+            for (move7 in b$legalMoves) {
+              b <- makeMove(b, move7)
+              if (b$isGameOver) {
+                print(b, full = T)
+                b <- undoMove(b,move7)
+                next
+              }
+              for (move8 in b$legalMoves) {
+                b <- makeMove(b, move8)
+                if (b$isGameOver) {
+                  cat(b$moves)
+                  cat("\n")
+                  b <- undoMove(b,move8)
+                  next
+                }
+                for (move9 in b$legalMoves) {
+                  b <- makeMove(b, move9)
+                  cat(b$moves)
+                  cat("\n")
+                  b <- undoMove(b,move5)
+                } # move 9
+              } # move 8
+            } # move 7
+          } # move 6
+        } # move 5
+      } # move 4
+    } # move 3
+  } # move 2
+} # move 1
+      
---
+++ TicTacToeBoard.R
@@ -0,0 +1,142 @@
+makeMove <- function(obj, move) { UseMethod("makeMove") }
+isGameOver <- function(obj) { UseMethod("isGameOver") }
+undoMove <- function(obj, move) { UseMethod("undoMove") }
+
+TicTacToeBoard <- function() {
+	value <- list(whoToMove = "X", 
+		      isGameOver = FALSE, 
+		      winner = NA,
+		      board = rep("-",9),
+		      legalMoves = c("A1","B1","C1","A2","B2","C2","A3","B3","C3"),
+		      moves <- list()
+		     )
+	attr(value, "class") <- "TicTacToeBoard"
+	value
+}
+
+undoMove.TicTacToeBoard <- function(obj, move) {
+  row <- switch(substr(move,1,1), A = 0, B = 1, C = 2)
+  col <- as.integer(substr(move,2,2))
+  pos <- (3 * row + col)
+  if (obj$board[[pos]] == "-") return(obj)
+  obj$moves <- obj$moves[-length(obj$moves)]
+  obj$board[[pos]] <- "-"
+
+  if (obj$whoToMove == "X") {
+    obj$whoToMove <-  "O"
+  } else {
+    obj$whoToMove <- "X"
+  }
+  obj$legalMoves <- append(obj$legalMoves, move)
+  obj <- isGameOver(obj)
+  obj
+}
+
+isGameOver.TicTacToeBoard <- function(obj) {
+	if (obj$isGameOver) { return(obj$winner) }
+	if (length(obj$legalMoves) == 0) {
+		obj$isGameOver <- TRUE
+		obj$winner <- "-"
+	}
+	for (j in 0:2) {
+		x1 <- 0; o1 <- 0; x2 <- 0; o2 <- 0
+		for (i in 0:2) {
+			pos1 <- i+j*3+1
+			pos2 <- j+i*3+1
+			if (obj$board[[pos1]] == "X") { x1 <- x1 + 1 }
+			if (obj$board[[pos1]] == "O") { o1 <- o1 + 1 }
+			if (obj$board[[pos2]] == "X") { x2 <- x2 + 1 }
+			if (obj$board[[pos2]] == "O") { o2 <- o2 + 1 }
+		}
+		if (x1== 3 || x2 == 3) {
+			obj$isGameOver <- TRUE
+			obj$winner <- "X"
+		}
+		if (o1== 3 || o2 == 3) {
+			obj$isGameOver <- TRUE
+			obj$winner <- "O"
+		}
+	}
+	if (obj$board[[1]] == obj$board[[5]] && 
+	    obj$board[[5]] == obj$board[[9]] &&
+	    obj$board[[1]] == "X") {
+		obj$isGameOver <- TRUE
+		obj$winner <- "X"
+	}
+	if (obj$board[[1]] == obj$board[[5]] && 
+	    obj$board[[5]] == obj$board[[9]] &&
+	    obj$board[[1]] == "O") {
+		obj$isGameOver <- TRUE
+		obj$winner <- "O"
+	}
+	if (obj$board[[3]] == obj$board[[5]] && 
+	    obj$board[[5]] == obj$board[[7]] &&
+	    obj$board[[3]] == "X") {
+		obj$isGameOver <- TRUE
+		obj$winner <- "X"
+	}
+	if (obj$board[[3]] == obj$board[[5]] && 
+	    obj$board[[5]] == obj$board[[7]] &&
+	    obj$board[[3]] == "O") {
+		obj$isGameOver <- TRUE
+		obj$winner <- "O"
+	}
+	obj
+}
+
+print.TicTacToeBoard <- function(obj, full = FALSE) {
+	cat(paste0("A ",obj$board[[1]],obj$board[[2]],obj$board[[3]],"\n"))
+	cat(paste0("B ",obj$board[[4]],obj$board[[5]],obj$board[[6]],"\n"))
+	cat(paste0("C ",obj$board[[7]],obj$board[[8]],obj$board[[9]],"\n"))
+	cat(paste0("  123\n"))
+	cat("\n")
+	if (full) {
+	  cat("Moves: ",obj$moves,"\n")
+	  cat("Who to move: ",obj$whoToMove,"\n")
+	  cat("isGameOver: ",obj$isGameOver,"\n")
+	  cat("winner: ",obj$winner,"\n")
+	}
+}
+
+makeMove.TicTacToeBoard <- function(obj, move) {
+	if (obj$isGameOver) return(obj)
+	obj$moves <- append(obj$moves, move)
+	row <- switch(substr(move,1,1), A = 0, B = 1, C = 2)
+	col <- as.integer(substr(move,2,2))
+	pos <- (3 * row + col)
+	if (obj$board[[pos]] != "-") return(obj)
+	if (obj$whoToMove == "X") {
+		obj$board[[pos]] <- "X"
+		obj$whoToMove <-  "O"
+	} else {
+		obj$board[[pos]] <- "O"
+		obj$whoToMove <- "X"
+	}
+	obj$legalMoves <- obj$legalMoves[obj$legalMoves != move]
+	obj <- isGameOver(obj)
+	obj
+}
+
+playRandomGames <- function(num = 1) {
+  replicate(num, {
+    b <- TicTacToeBoard()
+    while (b$isGameOver == FALSE) {
+      aMove <- sample(b$legalMoves,1)
+      b <- makeMove(b, aMove)
+    }
+    b$moves
+  })
+}
+
+unittest <- function() {
+  b <- TicTacToeBoard()
+  print(b)
+  b <- makeMove(b,"A2")
+  b <- undoMove(b, "A2")
+  print(b, full = T)
+  b <- makeMove(b,"A2")
+  print(b, full = T)
+}
+
+unittest()
+
