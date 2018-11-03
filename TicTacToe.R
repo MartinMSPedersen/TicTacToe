e30f74ce79f4916a73404efd9f2d719621c4f5c2
Martin M. S. Pedersen <traxplayer@gmail.com>
2018-11-03 20:39:22 +0100

    x

--- TicTacToe.R
+++ TicTacToe.R
@@ -1,12 +1,19 @@
+library(magrittr)
+
 source("TicTacToeBoard.R")
 
-
-makeAllGames <- function() {
+makeAllGames <- function(output = FALSE) {
+	result <- vector("character", 260000)
+	idx <- 1
 	placeOne <- function(b) {
 		for (move in b$legalMoves) {
 			b <- makeMove(b, move)
 			if (b$isGameOver) {
-				cat(paste0(b$moves, collapse = ""),"\n")
+				if (output) {
+					cat(paste0(b$moves, collapse = ""),"\n")
+				}
+				result[idx] <<- paste0(b$moves, collapse = "")
+				idx <<- idx + 1
 			} else {
 				placeOne(b)
 			}
@@ -14,27 +21,40 @@ makeAllGames <- function() {
 		}
 	}
 	placeOne(TicTacToeBoard())
+	result[nchar(result) != 0]
 }
 
-makeAllPositions <- function() {
+makeAllPositions <- function(output = FALSE) {
+	result <- vector("character", 6e5)
+	b <- TicTacToeBoard()
+        result[1] <- getPosition(b)	
+	idx <- 2
+	if (output) {
+		cat(getPosition(b),"\n")
+	}
+
 	placeOne <- function(b) {
 		for (move in b$legalMoves) {
 			b <- makeMove(b, move)
-			cat(getPosition(b),"\n")
+			if (output) {
+				cat(getPosition(b),"\n")
+			}
+			result[idx] <<- getPosition(b)
+			idx <<- idx + 1
 			if (!b$isGameOver) {
 				placeOne(b)
 			}
 			b <- undoMove(b, move)
 		}
 	}
-	b <- TicTacToeBoard()
-	cat(getPosition(b),"\n")
 	placeOne(b)
+	result <- unique(result)
+	result[nchar(result) != 0]
 }
 
+games <- makeAllGames(T)	
+positions <- makeAllPositions(T)
+	
 
-
-#makeAllGames()	
-makeAllPositions()	
 
 
