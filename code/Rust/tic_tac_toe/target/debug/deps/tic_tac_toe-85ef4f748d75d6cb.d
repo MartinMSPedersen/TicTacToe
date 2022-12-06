1fd6e1b03a0f75a43298b56c3e8f495a2a34aaf8
Martin M. S. Pedersen <mp@superusers.dk>
2022-12-06 17:38:44 +0100

    Rust version - wip

---
+++ code/Rust/tic_tac_toe/target/debug/deps/tic_tac_toe-85ef4f748d75d6cb.d
@@ -0,0 +1,5 @@
+/home/mm/gits/TicTacToe/code/Rust/tic_tac_toe/target/debug/deps/tic_tac_toe-85ef4f748d75d6cb: src/main.rs
+
+/home/mm/gits/TicTacToe/code/Rust/tic_tac_toe/target/debug/deps/tic_tac_toe-85ef4f748d75d6cb.d: src/main.rs
+
+src/main.rs:
---
+++ code/Rust/tic_tac_toe/target/debug/tic_tac_toe.d
@@ -0,0 +1,1 @@
+/home/mm/gits/TicTacToe/code/Rust/tic_tac_toe/target/debug/tic_tac_toe: /home/mm/gits/TicTacToe/code/Rust/tic_tac_toe/src/lib.rs /home/mm/gits/TicTacToe/code/Rust/tic_tac_toe/src/main.rs
